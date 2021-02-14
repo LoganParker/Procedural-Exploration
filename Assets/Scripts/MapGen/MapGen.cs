60d438a8d2ef5f4c14312a954792628e6d3917f9
LoganParker <logandavidparker@gmail.com>
2021-02-13 17:49:37 -0700

    Implemented Regions and Color

--- Assets/Scripts/MapGen/MapGen.cs
+++ Assets/Scripts/MapGen/MapGen.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour
 {
+
+    public enum DrawMode{NoiseMap,ColorMap}
+    [SerializeField]private DrawMode drawMode;
     public bool autoUpdate;
 
     //Map Parameters
@@ -16,13 +19,40 @@ public class MapGen : MonoBehaviour
     [SerializeField]private float lacunarity;    
     [SerializeField]private Vector2 offset;
     //
+    
+    public TerrainType[] regions;
 
+    //Generates our map
     public void GenerateMap(){
+        // Get our noise map
         float[,] noiseMap = NoiseGen.GenerateNoiseMap(mapWidth,mapHeight,seed,noiseScale,octaves,persistance,lacunarity,offset);
+        
+        //Assign Terrain based on height
+        Color[] colorMap = new Color[mapWidth*mapHeight];
+        for(int x = 0;x < mapWidth; x++){
+            for(int y = 0;y < mapHeight; y++){
+                float currentHeight = noiseMap[x,y];
 
+                for(int i = 0;i < regions.Length; i++){
+                    if(currentHeight<= regions[i].height){
+                        colorMap[y*mapWidth+x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        
+        if(drawMode==DrawMode.NoiseMap){
+            display.DrawTexture(TextureGen.TextureFromHeightMap(noiseMap));
+        }else if (drawMode == DrawMode.ColorMap){
+            display.DrawTexture(TextureGen.TextureFromColorMap(colorMap,mapWidth,mapHeight));
+        }
+        
     }
+    
+
+    //Ensure correct Values
     private void OnValidate() {
         if(mapWidth < 1){
             mapWidth = 1;
@@ -38,4 +68,11 @@ public class MapGen : MonoBehaviour
         }
         
     }
+}
+
+[System.Serializable] 
+public struct TerrainType{
+    public string name;
+    public float height;
+    public Color color;
 }
